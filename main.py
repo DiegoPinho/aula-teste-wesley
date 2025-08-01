8c8e42258fd9c59e7ce632ef076017ec73337e0f
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-08-01 16:24:23 -0300

    versao 2

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Versão 1")
+print("Versão 2")
 
 ## git init => inicializa o repositório
 ## git status => mostrar o status dos arquivos dentro do seu repositório
+## git add . => adiciona todos os arquivos para o stage (pedindo pra ele ser versionado / salvo)
+## git commit -m "mensagem" => cria um commit com a mensagem informada
+
+
+## commit => versão do seu código
