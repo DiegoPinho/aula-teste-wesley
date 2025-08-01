a7d74ceb373d47158f6431c1a40b2596cc6ee654
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-08-01 16:57:04 -0300

    versao 6

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-print("Versão 5")
+print("Versão 6")
 
 ## git init => inicializa o repositório
 ## git status => mostrar o status dos arquivos dentro do seu repositório
 ## git add . => adiciona todos os arquivos para o stage (pedindo pra ele ser versionado / salvo)
 ## git commit -m "mensagem" => cria um commit com a mensagem informada
 ## git log => mostra o histórico de commits
+## git push => envia os commits para o repositório remoto (ex: GitHub)
 ## salvar meu repo no github
 
 
